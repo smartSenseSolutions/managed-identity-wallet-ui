af5cadbed820d22ab7d5d5a2ceff7e71af81ade5
deepak.kumar38 <deepak.kumar@smartsensesolutions.com>
2023-08-16 18:29:31 +0530

    fix:type

--- src/APIs/VcManagement.api.ts
+++ src/APIs/VcManagement.api.ts
@@ -75,7 +75,7 @@ export const postIssueMembership = (param: { bpn: string }) => {
     return post(ENDPOINTS.postIssueMembership, param);
 };
 export const postIssueFramework = (param: {
-    holderIdentifier: string;
+    holderIdentifier: string | undefined;
     type: string;
     'contract-template': string;
     'contract-version': string;
